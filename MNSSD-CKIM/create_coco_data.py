8e0752d02ed378ef2bfae58ea5755021ab828bc5
puzhang1993 <129584317+puzhang1993@users.noreply.github.com>
2023-04-01 20:42:24 +0800

    Delete MNSSD-CKIM directory

--- MNSSD-CKIM/create_coco_data.py
+++
@@ -1,5 +0,0 @@
-
-from coco_utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(coco_val_path='train2017', output_folder='./') #反正就是两个文件夹coco_train_path='val2017', 
--- MNSSD-CKIM/create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(voc07_path='merge2007/VOC2007', voc12_path='merge2012/VOC2012', output_folder='./') #反正就是两个文件夹
-    # create_data_lists(voc07_path='merge2007/VOC2007', voc12_path='merge2012/VOC2012', output_folder='./') #反正就是两个文件夹
-
